bc79dd6bae4f5525fc7e5c6065e803cdcaee60fe
Hana Ramdhani <56961607+hanaramdhani@users.noreply.github.com>
2020-10-08 05:03:57 -0700

    Add files via upload

---
+++ web2/make_cv.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+<title>CV Saya</title>
+<style>
+    td{
+      border-collapse:collaspse;
+      padding: 15px;  
+    }
+</style>
+</head>
+<body>
+<!-- <img src="gbr1.jpg" alt=""> -->
+<h1 align="center">Welcome to My Home Page</h1>
+<hr>
+<div class="container">
+<div class="row">
+    <div class="col-md-6"><img class="img-square" src="hana1.jpg" alt="_blank" width='400px'></div>
+    <div class="col-md-6">
+        <h2 align="CENTER" style="color:blue">CURRICULUM VITAE</h2>
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>Hana Ramdhani</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>:</td>
+                <td>Teknik Informatika</td>
+            </tr>
+            <tr>
+                <td>Universitas</td>
+                <td>:</td>
+                <td>Universitas Bumigora</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>:</td>
+                <td>1810530121</td>
+            </tr>
+            <tr>
+                <td rowspan="2" valign="top">Alamat</td>
+                <td rowspan="2" valign="top">:</td>
+                <td>Dompu</td>
+            </tr>
+            <tr>
+                <td>ffffff</td>
+            </tr>
+            <tr>
+                <td>Tempat Tanggal Lahir:</td>
+                <td>:</td>
+                <td>Bogor, 13 Desember 2000</td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+</div>
+</body>
+
+</html>
